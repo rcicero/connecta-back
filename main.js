d28c630e1fd5e386661f79e9e92e106a9d6a6289
marcospf-eng <marcospf.eng@gmail.com>
2018-06-09 23:07:00 -0300

    Added api which do request to elastic search.

--- main.js
+++ main.js
@@ -1,33 +1,61 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+let rp = require('request-promise');
 let app = express()
 
 // Basic configuration
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.listen(8080, function() {
+app.listen(8080, () => {
 	console.log("Server running.")
 })
 
-// Business logic
+// Infrastructure
 const map_to_array = (map) => {
 	let list = []
 	Object.keys(map).forEach(key => list.push(map[key]))
 	return list
 }
 
-// Map data
+function getSuccessResponseModel(data) {
+	return {status: 200, result: data}
+}
+
+function getErrorResponseModel(error) {
+	return {status: error.statusCode, result: error.message}
+}
+
+function createGetOptions(endpoint, param) {
+	return {
+    uri: 'http://40.114.27.240:9200/' + endpoint + param,
+    json: true
+	};
+}
+
+// Map endpoints data
 const endpoints = {
-	1: {id: 1, api: 'home'},
-	2: {id: 2, api: 'api'}
+	0: {id: 0, api: 'home'},
+	1: {id: 1, api: 'user'}
 }
 
 const home = (res) =>	res.send('Hello ;)')
-const list_endpoints = (res) => res.json(map_to_array(endpoints))
+const success_response = (res, data) => res.json(getSuccessResponseModel(data))
+const error_response = (res, error) => res.json(getErrorResponseModel(error))
 
-// Routing
+// Routing. E.g.: app.put('/api/name/:id', (req, res) => myfunction(req.params.id, req.body.extras, res))
 app.get('/', (req, res) => home(res))
 
 app.get('/api', (req, res) => list_endpoints(res))
 
-//app.put('/api/name/:id', (req, res) => myfunction(req.params.id, req.body.extras, res))
+app.get('/api/user/:id', (req, res) => getUserAccount(res, req.params.id))
+
+// server requests
+function getUserAccount(res, userId) {
+	rp(createGetOptions("accounts/account/", userId))
+    .then(function (promisseResponse) {
+        success_response(res, promisseResponse)
+    })
+    .catch(function (error) {
+      	error_response(res, error)
+    });
+}
